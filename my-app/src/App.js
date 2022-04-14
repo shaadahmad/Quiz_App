3b668630af48f59e572bec5168f1cd10794a7a1a
Shaad Ahmad <54548823+shaadahmad@users.noreply.github.com>
2022-04-14 12:18:27 +0530

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,37 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
-import {Provider} from 'react-redux'
+import { Provider } from 'react-redux'
 import store from './redux/store';
-import CakeCont from './component/cakeCont';
-import HooksCake from './component/HooksCake'
-import iceCont from './component/IceCont';
-import IceCont from './component/IceCont';
-import NewCakeContainer from './component/NewCakeContainer';
 import UserContainer from './component/UserContainer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Register from './component/Register';
 import Login from './component/Login';
+import ProtectedPage from './component/ProtectedPage';
+import AdminPage from './component/AdminPage';
+import ProtectAdminPage from './component/ProtectAdminPage';
+import NewQuestions from './component/NewQuestions';
+import ProtectNewQuestion from './component/ProtectNewQuestion';
+
+
 
 
 function App() {
   return (
     <Provider store={store}>
-    <div className="App">
-      <BrowserRouter>
-      <Routes>
-  <Route path='/' element={<Register/>}></Route>
-  <Route path='/Quiz' element={<UserContainer/>}></Route>
-  <Route path='/login' element={<Login/>}></Route>
-
-      </Routes>
-      </BrowserRouter>
-      {/* <HooksCake/> */}
-      {/* <CakeCont /> */}
-     {/* <IceCont/> */}
-     {/* <NewCakeContainer/> */}
-     {/* <Register/>
-     <UserContainer/> */}
-    </div>
+      <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Register />}></Route>
+            <Route path='/Quiz' element={<ProtectedPage><UserContainer /></ProtectedPage>}></Route>
+            <Route path='/admin' element={<ProtectAdminPage><AdminPage /></ProtectAdminPage>}></Route>
+            <Route path='/add' element={<ProtectNewQuestion><NewQuestions /></ProtectNewQuestion>}></Route>
+            <Route path='/login' element={<Login />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
     </Provider>
   );
 }
