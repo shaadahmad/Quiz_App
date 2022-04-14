387813f61d7c9e79bc9e289c1abb33a5279f80cc
Shaad Ahmad <54548823+shaadahmad@users.noreply.github.com>
2022-04-14 12:20:29 +0530

    Update R_Reducer.js

--- my-app/src/redux/Register/R_Reducer.js
+++ my-app/src/redux/Register/R_Reducer.js
@@ -1,43 +1,56 @@
 import { REGISTER_USER_FAILURE, REGISTER_USER_REQUEST, REGISTER_USER_SUCCESS } from "./R_type"
 
-const initialState={
-    loading:false,
-    users:[],
-    error:'',
-    active:""
-
+const initialState = {
+    loading: false,
+    users: [],
+    error: '',
+    active: "",
+    authenticate: false,
+    nameOfAdmin:"",
+    reportCard:[]
 }
 
-const reducer=(state=initialState,action)=>
-{
-    switch(action.type)
-    {
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
         case REGISTER_USER_REQUEST:
-            return{
+            return {
                 ...state,
-                loading:true
-
+                loading: true
             }
 
-            case REGISTER_USER_SUCCESS:
-            return{
-                loading:false,
-                users:action.payload,
-                error:''
-                
+        case REGISTER_USER_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                users: action.payload,
+                error: ''
             }
-            case REGISTER_USER_FAILURE:
-            return{
-                loading:false,
-                users:[],
-                error:action.payload
-                
+        case REGISTER_USER_FAILURE:
+            return {
+                ...state,
+                loading: false,
+                users: [],
+                error: action.payload
             }
-            case "active":
-                return{
-                    ...state,active:action.payload
-                }
-            default: return state
+        case "active":
+            return {
+                ...state, active: action.payload
+            }
+
+        case "userIsLogin":
+            return {
+                authenticate: true
+            }
+            case "adminName":
+                console.log(action.payload,"reducer")
+            return {
+                ...state, nameOfAdmin: action.payload
+            }
+            case "report":
+            return {
+                ...state, reportCard: action.payload
+            }
+        default: return state
     }
 }
 export default reducer
